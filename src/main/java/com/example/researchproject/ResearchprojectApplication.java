09a4a2277bea214efc98b872e4a98fe5de12631e
shareeb hashmi <shareeb.hashmi@student.howest.be>
2025-01-06 15:19:27 +0100

    Initial commit

---
+++ src/main/java/com/example/researchproject/ResearchprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.researchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/researchproject/ResearchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.researchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
